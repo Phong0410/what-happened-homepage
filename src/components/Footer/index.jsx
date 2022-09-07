e7189722d3e33639b6c8d34c3206911e06284903
Phong0410 <17520895@gm.uit.edu.vn>
2022-09-07 17:36:27 +0700

    delete unused images and add pointer for links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -12,16 +12,16 @@ const Footer = () => {
         <CContainer fluid>
           <span className="text-style-15 text-blackTwo">What happened</span>
           <ul>
-            <li className="text-style-26 w-60 pt-2">
+            <li className="text-style-26 w-60 pt-2 cursor-pointer">
               [공지] 개인 정보 처리 방침 변경 사전 안내
             </li>
-            <li className="text-style-26 w-60 pt-2">
+            <li className="text-style-26 w-60 pt-2 cursor-pointer">
               [공지] 29CM 강남 스토어 영업 종료
             </li>
-            <li className="text-style-26 w-60 pt-2">
+            <li className="text-style-26 w-60 pt-2 cursor-pointer">
               [공지] iOS 10 이하 버전 지원 중단 안내
             </li>
-            <li className="text-style-26 w-60 pt-2">
+            <li className="text-style-26 w-60 pt-2 cursor-pointer">
               [공지] 개인 정보 처리 방침 변경 사전 안내
             </li>
           </ul>
@@ -31,38 +31,58 @@ const Footer = () => {
             <CCol>
               <span className="text-style-15 text-blackTwo">About us</span>
               <ul>
-                <li className="text-style-26 pt-2">회사 소개</li>
-                <li className="text-style-26 pt-2">인재 채용</li>
-                <li className="text-style-26 pt-2">상시 할인 혜택</li>
+                <li className="text-style-26 pt-2 cursor-pointer">회사 소개</li>
+                <li className="text-style-26 pt-2 cursor-pointer">인재 채용</li>
+                <li className="text-style-26 pt-2 cursor-pointer">
+                  상시 할인 혜택
+                </li>
               </ul>
             </CCol>
             <CCol>
               <span className="text-style-15 text-blackTwo">My order</span>
               <ul>
-                <li className="text-style-26 pt-2">내 주문</li>
-                <li className="text-style-26 pt-2">주문 배송</li>
-                <li className="text-style-26 pt-2">취소 / 교환 / 반품 내역</li>
-                <li className="text-style-26 pt-2">상품 리뷰 내역</li>
-                <li className="text-style-26 pt-2">증빙 서류 발급</li>
+                <li className="text-style-26 pt-2 cursor-pointer">내 주문</li>
+                <li className="text-style-26 pt-2 cursor-pointer">주문 배송</li>
+                <li className="text-style-26 pt-2 cursor-pointer">
+                  취소 / 교환 / 반품 내역
+                </li>
+                <li className="text-style-26 pt-2 cursor-pointer">
+                  상품 리뷰 내역
+                </li>
+                <li className="text-style-26 pt-2 cursor-pointer">
+                  증빙 서류 발급
+                </li>
               </ul>
             </CCol>
             <CCol>
               <span className="text-style-15 text-blackTwo">My account</span>
               <ul>
-                <li className="text-style-26 pt-2">회원 정보 수정</li>
-                <li className="text-style-26 pt-2">회원 등급</li>
-                <li className="text-style-26 pt-2">마일리지 현황</li>
-                <li className="text-style-26 pt-2">쿠폰</li>
+                <li className="text-style-26 pt-2 cursor-pointer">
+                  회원 정보 수정
+                </li>
+                <li className="text-style-26 pt-2 cursor-pointer">회원 등급</li>
+                <li className="text-style-26 pt-2 cursor-pointer">
+                  마일리지 현황
+                </li>
+                <li className="text-style-26 pt-2 cursor-pointer">쿠폰</li>
               </ul>
             </CCol>
             <CCol>
               <span className="text-style-15 text-blackTwo">Help</span>
               <ul>
-                <li className="text-style-26 pt-2">1 : 1 상담 내역</li>
-                <li className="text-style-26 pt-2">상품 Q & A 내역</li>
-                <li className="text-style-26 pt-2">공지 사항</li>
-                <li className="text-style-26 pt-2">자주하는 질문</li>
-                <li className="text-style-26 pt-2">고객의 소리</li>
+                <li className="text-style-26 pt-2 cursor-pointer">
+                  1 : 1 상담 내역
+                </li>
+                <li className="text-style-26 pt-2 cursor-pointer">
+                  상품 Q & A 내역
+                </li>
+                <li className="text-style-26 pt-2 cursor-pointer">공지 사항</li>
+                <li className="text-style-26 pt-2 cursor-pointer">
+                  자주하는 질문
+                </li>
+                <li className="text-style-26 pt-2 cursor-pointer">
+                  고객의 소리
+                </li>
               </ul>
             </CCol>
           </CRow>
@@ -84,9 +104,9 @@ const Footer = () => {
           </p>
         </div>
         <div className="flex items-center mt-2">
-          <img src={snsIcon01} alt="icon" className="w-6 h-6" />
-          <img src={snsIcon02} alt="icon" className="w-6 h-6" />
-          <img src={snsIcon03} alt="icon" className="w-6 h-6" />
+          <img src={snsIcon01} alt="icon" className="w-6 h-6 cursor-pointer" />
+          <img src={snsIcon02} alt="icon" className="w-6 h-6 cursor-pointer" />
+          <img src={snsIcon03} alt="icon" className="w-6 h-6 cursor-pointer" />
         </div>
       </CContainer>
     </CContainer>
